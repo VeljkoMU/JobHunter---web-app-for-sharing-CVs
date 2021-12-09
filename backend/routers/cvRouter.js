b616bf07564b8d4eba78f60578e5b5293c36e41b
Weki <wekizator@elfak.rs>
2021-12-09 17:16:29 +0100

    Popravljmo bekend

--- backend/routers/cvRouter.js
+++ backend/routers/cvRouter.js
@@ -46,43 +46,43 @@ cvRouter.put("/submit", async (req, res)=>{
     }
 
     // Provera da li dati user vec ima cv
-    await sql.query(`select cv.id from cv join user on cv.id where username= ${req.session.user}`, (err, rows, fileds)=>{
+    await sql.query(`select cv.id from cv inner join user on cv.id = user.cv where username= "${req.session.user}"`, async (err, rows, fileds)=>{
+        console.log(rows);
         if(rows.length!=0)
-            existringCv = rows[0];
-    });
+            existringCv = rows[0].id;
+    
 
     // Ako ima, updatujemo ga 
     if(existringCv){
         sqlUpdateQuery = `
             update cv set
-            name= ${req.body.name},
-            surname = ${req.body.surname},
-            biography = ${req.body.bio},
-            education = ${req.body.education},
-            employment_history = ${req.body.work},
-            email = ${req.body.email},
+            name= "${req.body.name}",
+           surname = "${req.body.surname}",
+            biography = "${req.body.bio}",
+            education = "${req.body.education}",
+            employment_history = "${req.body.work}",
+            email = "${req.body.email}",
             phone_number = ${req.body.num}
             where cv.id = ${existringCv};
         `;
 
-        mysql.query(sqlUpdateQuery);
+        sql.query(sqlUpdateQuery);
+        res.status(200).end();
+        return;
         // Vidi sta ces za dodavanje skillova
     }
     else{
         // Ako nema pravimo novi
         let categoryId = null;
-        sqlInsertIntoQuery = `
-            insert into cv  (name, surname, biography, education, employment_history, email, phone_number, category)
-            values (${req.body.name}, ${req.body.surname}, ${req.body.bio}, ${req.body.education}, ${req.body.work}, ${req.body.email}, ${req.body.num}, ${categoryId});
-        `;
+
 
         sqlGetCategoryId = `
-            select category.id from category where category.name = ${req.body.category};
+            select category.id from category where category.name = "${req.body.category}";
         `;
         let createCvId = null;
 
         // Provedi da li kategorija postoji i ako postoji dodaj je u cv
-        await sql.query(sqlGetCategoryId, (err, result, f)=>{
+        await sql.query(sqlGetCategoryId, async (err, result, f)=>{
             if(err){
                 console.log(err);
                 res.status(500).end();
@@ -94,11 +94,13 @@ cvRouter.put("/submit", async (req, res)=>{
                 return;
             }
 
-            categoryId = result[0];
-        });
-
-        // Dodaj cv u bazu i zapamti njegov id
-        await sql.query(sqlInsertIntoQuery, (err, result, f)=>{
+            categoryId = result[0].id;
+            console.log(categoryId);
+            sqlInsertIntoQuery = `
+            insert into cv  (name, surname, biography, education, employment_history, email, phone_number, category)
+            values ("${req.body.name}", "${req.body.surname}", "${req.body.bio}", "${req.body.education}", "${req.body.work}", "${req.body.email}", ${req.body.num}, ${categoryId});
+        `;
+        await sql.query(sqlInsertIntoQuery, async (err, result, f)=>{
             if(err){
                 console.log(err);
                 res.status(500).end();
@@ -106,13 +108,24 @@ cvRouter.put("/submit", async (req, res)=>{
             }
 
             createCvId = result.insertId;
+            await addSkillsToCv(req.body.skills, createCvId, res);
+
+            await sql.query(`update user set
+                            cv = ${createCvId}
+                            where username="${req.session.user}";
+                `);
+        });
         });
 
+        // Dodaj cv u bazu i zapamti njegov id
+
+
         // Funkcija koja dodaje skillove cv-u, ako postoje samo doaj u has_skill, ako ne postoje dodaj novi skill i povezi ga za cv
-        await addSkillsToCv(req.body.skills, createCvId, res);
+
 
         res.status(200).end();
     }
+});
 });
 
 cvRouter.get("/search", async (req, res)=>{
@@ -140,6 +153,24 @@ cvRouter.get("/search", async (req, res)=>{
 
 });
 
+cvRouter.get("/getCv", async (req, res)=>{
+    if(!req.session.authorized){
+        res.status(403).end();
+        return;
+    }
+
+    console.log(req.session.user);
+    await sql.query(`select * from (cv inner join user on user.cv=cv.id) where user.username = "${req.session.user}";`, (err, result, f)=>{
+        if(err){
+            console.log(err);
+            res.status(500).end();
+            return;
+        }
+        console.log(result);
+        res.json(result[0]).end();
+        return;
+    });
+});
 async function getDataArray(redisKey, SQLQuery){
     // Uzima podatak iz redisa ili ako ga nema u redisu uzima iz sql-a i upisuje u redis
     return new Promise(async (resolve, reject)=>{
@@ -184,21 +215,18 @@ async function addSkillsToCv(skills, cvId, res){
     // Sara proveri ovu funkciju, nzm dal je dobar sql :(
     let skill = null;
     let skillId = null;
-    sqlGetSkillId = `
-        select skill.id from skill where skill.name = ${skill};
-    `;
-    sqlInsertSkill = `
-        insert into skill (name)
-        values (${skill});
-    `;
-    sqlLinkSkillToCv = `
-        insert into has_skill (cv, skill)
-        values (${cvId}, ${skillId});
-    `;
+
+
+
 
     await skills.forEach( async (s)=>{
+        console.log(s);
         skill = s;
 
+
+    sqlGetSkillId = `
+    select skill.id from skill where skill.name = "${skill}";
+    `;
         await sql.query(sqlGetSkillId, async (err, result, f)=>{
             if(err){
                 console.log(err);
@@ -207,21 +235,38 @@ async function addSkillsToCv(skills, cvId, res){
             }
 
             if(result.length===0){
+                sqlInsertSkill = `
+                insert into skill (name)
+                values ("${skill}");
+            `;
                 await  sql.query(sqlInsertSkill, async (err, resultSkill, f)=>{
+                    console.log(sqlInsertSkill);
                     if(err){
                         console.log(err);
                         res.status(500).end();
                         return;
                     }
 
-                    skillId = resultSkill.resultId;
+                    skillId = resultSkill.insertId;
+                    console.log(skillId);
+                    sqlLinkSkillToCv = `
+                    insert into has_skill (cv, skill)
+                    values (${cvId}, ${skillId});
+                `;
+                   await sql.query(sqlLinkSkillToCv);
                 });
             }
             else{
-                skillId = resultSkill[0];
+                skillId = result[0].id;
+                console.log(skillId);
+                sqlLinkSkillToCv = `
+                insert into has_skill (cv, skill)
+                values (${cvId}, ${skillId});
+            `;
+               await sql.query(sqlLinkSkillToCv);
             }
 
-           await sql.query(sqlLinkSkillToCv);
+
         });
     });
 }
@@ -234,13 +279,13 @@ cvRouter.post("/skill", async (req, res)=>{
         return;
     }
 
-    await sql.query(`select cv.id from cv join user on cv.id = user.cv where user.username = "${req.session.username}"`, async (err, r, f)=>{
+    await sql.query(`select cv.id from cv inner join user on cv.id = user.cv where user.username = "${req.session.user}"`, async (err, r, f)=>{
         if(err){
             res.status(500).end();
             return;
         }
-
-        await addSkillsToCv([skill], r[0], res);
+        console.log(r);
+        await addSkillsToCv([skill], r[0].id, res);
     });
 
     res.status(200).end();
@@ -255,13 +300,13 @@ cvRouter.delete("/skill", async (req, res)=>{
         return;
     }
 
-    await sql.query(`select cv.id from cv join user on cv.id = user.cv where user.username = "${req.session.username}"`, async (err, r, f)=>{
+    await sql.query(`select cv.id from cv inner join user on cv.id = user.cv where user.username = "${req.session.user}"`, async (err, r, f)=>{
         if(err){
             res.status(500).end();
             return;
         }
 
-        await sql.query(`delete from has_skill where cv = "${r[0]}" and skill = "${skill}"`);
+        await sql.query(`delete from has_skill where cv = ${r[0].id} and skill = (select id from skill where name= "${skill}");`);
     });
 
     res.status(200).end();
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,3 +1,4 @@
+const { ifError } = require('assert');
 const express = require('express');
 const session = require('express-session');
 const md5 = require('md5');
@@ -7,22 +8,24 @@ sql = require("../mysql");
 const userRouter = express.Router();
 
 userRouter.post("/login", async (req, res)=>{
-    let username = req.body.username;
-    let password = req.query.password;
-
-    await sql.query(`select * from user where user.username=${username};`, (err, result, f)=>{
+    let username = await req.body.username;
+    let password = await req.body.password;
+    console.log(username);
+    await sql.query(`select * from user where username="${username}";`, (err, result, f)=>{
         if(err){
             console.log(err);
             res.status(500).end();
             return;
         }
 
-        if(result.length!=0){
+        if(result.length==0){
             res.status(404).end()
             return;
         }
-
-        if(md5(password)===result[0].passowrd){
+        console.log(password);
+        console.log(result[0].passowrd);
+        console.log(md5(password).toString().slice(0, 19));
+        if(md5(password).toString().slice(0, 19)===result[0].password){
             req.session.authorized = true;
             req.session.user = username;
             res.status(200).end()
@@ -73,24 +76,23 @@ userRouter.delete("/deleteUser", async (req, res)=>{
 });
 
 userRouter.post("/register", async (req,res)=>{
-    username = req.body.username;
-    passowrd = req.body.password;
+    let username = await req.body.username;
+    let password = await req.body.password;
+    console.log(req.body);
     user = null;
-    await sql.query(`select * from user where user.username=${username};`, (err, result, f)=>{
-        if(err){
-            console.log(err);
-            res.status(500).end();
+    await sql.query(`select * from user where user.username=${req.body.username};`, (err, result, f)=>{
+        if(err)
             return;
-        }
-
+        
         if(result.length!=0){
             res.status(409).end()
             return;
         }
     });
 
+    console.log(password);
     await sql.query(`insert into user (username, password)
-                    values (${username}, ${md5(password)});  
+                    values ("${req.body.username}", "${md5(password).toString().slice(0,19)}");  
     `);
 
     res.status(200).end();
